b10676447456a7df79198903b2cb2e320b9cdf91
Сем Елена <semova.s81@bk.ru>
2022-10-23 11:53:26 +0300

    test ok

--- src/test/java/CallBankTest.java
+++ src/test/java/CallBankTest.java
@@ -1,48 +1,43 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.checkerframework.checker.nullness.qual.EnsuresNonNullIf;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriverInfo;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
+import org.openqa.selenium.chrome.ChromeOptions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallBankTest {
     private WebDriver driver;
-    //ChromeOptions options = new ChromeOptions();
-//options.addArguments("--disable-dev-shm-usage");
-//options.addArguments("--no-sandbox");
-//options.addArguments("--headless");
-    //driver = new ChromeDriver(options);
 
     @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "/drivers/win/chromedriver.exe");
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
     }
     @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
-
     @AfterEach
-    void tearDown() {
+    public void afterEach() {
         driver.quit();
         driver = null;
     }
     @Test
-    void shouldTestV1() IntrruptedException {
-        driver.get("http://localhost:9999/");
-        List<WebElement> input = driver.findElements(By.tagName("input"));
-        input.get(0).sendKeys("Ив Иван");
-        input.get(1).sendKeys("+79998888888");
-        driver.findElement(By.className("checkbox__control")).click();
-        driver.findElement(By.className("button__content")).click();
-        //driver.findElement(By.cssSelector("[data-test-id=phone]. input")).sendKeys("+79998888888");
-        String text = driver.findElement(By.className("[data-test-id=order-success]")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.");
+    void shouldTestV1() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ив Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998888888");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualText=driver.findElement(By.cssSelector("[data-test-id=order-success")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
     }
 
 }
